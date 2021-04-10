c171bed5910f4167bc862b28491d0f9a574ebcba
Jose Luis Herrera Jasso <jlhcorp2@hotmail.com>
2021-04-09 22:28:12 -0600

    [HMGR-22] Deleted some code not needed on user controller file

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,5 @@
 "use strict";
 const User = use("App/Models/User");
-const Home = use("App/Models/Home");
 class UserController {
   async create({ request, response, auth }) {
     const user = await User.create(
@@ -10,19 +9,17 @@ class UserController {
     // Find a user by user id and save needed values to home table
     const homeValues = await User.find(user.id);
     await homeValues.homes().create({ home_name: user.home });
-
     await auth.login(user);
-    return response.redirect("/");
+    return response.redirect("/", true);
   }
   async login({ request, auth, response, session }) {
     const { email, password } = request.all();
-
     try {
       await auth.attempt(email, password);
-      return response.redirect("/");
+      return response.redirect("/", true);
     } catch (error) {
       session.flash({ loginError: "Estas credenciales no funcionan." });
-      return response.redirect("/login");
+      return response.redirect("/login", true);
     }
   }
 }
