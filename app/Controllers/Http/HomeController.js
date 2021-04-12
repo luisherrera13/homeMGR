9229a2d7fa47b5138664f2adce4832315b0bb7b6
Jose Luis Herrera Jasso <jlhcorp2@hotmail.com>
2021-04-12 16:08:54 -0600

    [HMGR-30] Added filtering for user id on home query table to not need the templa index if contitional inside each iterator this to see if this same controller can be used for API requests since there is no view templates to make this filtering on it

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,13 +1,18 @@
 'use strict'
 //getting the model
 const Home = use('App/Models/Home')
+const User = use('App/Models/User')
 class HomeController {
-    async home({view}) {
-        // Fetch an item
-        const homes = await Home.all();
-        //const homeSpaces = await HomeSpace.query().where('home_id', 8).fetch();
+    async home({view, auth}) {
+        if(auth.user!==null){
+        const homeUser = await User.query().where('id', auth.user.id).fetch();
+        var homeUserid=homeUser.toJSON();        
+        const homes = await Home.query().where('user_id', homeUserid[0].id).fetch();
 
-        return view.render('index', { homes: homes.toJSON() })
+        return view.render('index', { homes: homes.toJSON() })}
+        else{
+            return view.render('index')
+        }
     }
 
 }
